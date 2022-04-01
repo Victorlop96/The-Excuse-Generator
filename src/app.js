c219f7cd4e2c3b76ce9afa809e82ec2d318f4c37
Victorlop96 <vlop1096@gmail.com>
2022-04-01 00:59:48 -0300

    finalizado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  var aleatorio1 = Math.floor(Math.random() * 4);
+  var aleatorio2 = Math.floor(Math.random() * 4);
+  var aleatorio3 = Math.floor(Math.random() * 3);
+  var aleatorio4 = Math.floor(Math.random() * 5);
+
+  document.getElementById(
+    "excuse"
+  ).innerHTML = `${who[aleatorio1]} ${action[aleatorio2]} ${what[aleatorio3]} ${when[aleatorio4]}`;
   console.log("Hello Rigo from the console!");
 };
